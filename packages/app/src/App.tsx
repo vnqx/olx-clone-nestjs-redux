0c5e67c5b3f4fc66528865b8386bcc5c7bc109a4
John <accs.x@pm.me>
2020-07-01 13:55:12 +0200

    Add basic components

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Header from "./Header";
 
 export default function App(): React.ReactElement {
-  return <div>eee edddee</div>;
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get("http://localhost:4000/postings");
+      console.log(response.data);
+    }
+
+    fetchData();
+  });
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<div>qweqwe</div>} />
+        <Route path="sign-up" element={<SignIn />} />
+        <Route path="sign-in" element={<SignUp />} />
+      </Routes>
+    </>
+  );
 }
